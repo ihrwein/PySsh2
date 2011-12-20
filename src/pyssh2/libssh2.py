8c674caa576a6601f4bdf86066e4eb534ad5b55f
Gellule Xg <gellule.xg@gmail.com>
2011-12-20 08:03:58 -1000

    Prefixed Session, Channel, Agent, and AgentPublicKey classes with LibSsh2.

--- src/pyssh2/libssh2.py
+++ src/pyssh2/libssh2.py
@@ -30,16 +30,16 @@ LIBSSH2_TRACE = {'TRANS'     : (1<<1),
                  'ALL'       : (1<<10)-1}
 
 
-class Session(Structure):
+class LibSsh2Session(Structure):
     pass
 
-class Channel(Structure):
+class LibSsh2Channel(Structure):
     pass
 
-class Agent(Structure):
+class LibSsh2Agent(Structure):
     pass
 
-class AgentPublicKey(Structure):
+class LibSsh2AgentPublicKey(Structure):
     pass
 
 
@@ -56,47 +56,47 @@ class LibSsh2:
         
         #========== LIBSSH2_SESSION
         #LIBSSH2_SESSION * libssh2_session_init_ex(LIBSSH2_ALLOC_FUNC((*myalloc)), LIBSSH2_FREE_FUNC((*myfree)), LIBSSH2_REALLOC_FUNC((*myrealloc)), void *abstract);
-        self.session_init_ex = self.setup('session_init_ex', [c_void_p, c_void_p, c_void_p, c_void_p], POINTER(Session))
+        self.session_init_ex = self.setup('session_init_ex', [c_void_p, c_void_p, c_void_p, c_void_p], POINTER(LibSsh2Session))
         #LIBSSH2_SESSION * libssh2_session_init(void);
         self.session_init = lambda : self.session_init_ex(None, None, None, None)
         #int libssh2_session_disconnect_ex(LIBSSH2_SESSION *session, int reason, const char *description, const char *lang);
-        self.session_disconnect_ex = self.setup('session_disconnect_ex', [POINTER(Session), c_int, c_char_p, c_char_p], c_int)
+        self.session_disconnect_ex = self.setup('session_disconnect_ex', [POINTER(LibSsh2Session), c_int, c_char_p, c_char_p], c_int)
         #int libssh2_session_disconnect(LIBSSH2_SESSION *session, const char *description);
         self.session_disconnect = lambda session, description: self.session_disconnect_ex(session, SSH_DISCONNECT['BY_APPLICATION'], description, "")
         #int libssh2_session_free(LIBSSH2_SESSION *session);
-        self.session_free = self.setup('session_free', [POINTER(Session)], c_int)
+        self.session_free = self.setup('session_free', [POINTER(LibSsh2Session)], c_int)
         #int libssh2_session_banner_set(LIBSSH2_SESSION *session, const char *banner);
-        self.session_banner_set = self.setup('banner_set', [POINTER(Session), c_char_p], c_int)
+        self.session_banner_set = self.setup('banner_set', [POINTER(LibSsh2Session), c_char_p], c_int)
         #void libssh2_trace(LIBSSH2_SESSION *session, int bitmask);
-        self.trace_set = self.setup('trace', [POINTER(Session), c_int], None)
+        self.trace_set = self.setup('trace', [POINTER(LibSsh2Session), c_int], None)
         #int libssh2_session_startup(LIBSSH2_SESSION *session, int socket);
-        self.session_startup = self.setup('session_startup', [POINTER(Session), c_int], c_int)
+        self.session_startup = self.setup('session_startup', [POINTER(LibSsh2Session), c_int], c_int)
         #void libssh2_session_set_blocking(LIBSSH2_SESSION *session, int blocking);
-        self.session_set_blocking = self.setup('session_set_blocking', [POINTER(Session), c_int], None)
+        self.session_set_blocking = self.setup('session_set_blocking', [POINTER(LibSsh2Session), c_int], None)
         
         #========== LIBSSH2_USERAUTH
         #char * libssh2_userauth_list(LIBSSH2_SESSION *session, const char *username, unsigned int username_len);
-        self.userauth_list = self.setup('userauth_list', [POINTER(Session), c_char_p, c_uint], c_char_p)
+        self.userauth_list = self.setup('userauth_list', [POINTER(LibSsh2Session), c_char_p, c_uint], c_char_p)
         #int libssh2_userauth_password_ex(LIBSSH2_SESSION *session,   const char *username,   unsigned int username_len,   const char *password,   unsigned int password_len,   LIBSSH2_PASSWD_CHANGEREQ_FUNC((*passwd_change_cb)));
-        self.userauth_password_ex = self.setup('userauth_password_ex', [POINTER(Session), c_char_p, c_uint, c_char_p, c_uint, c_void_p], c_int)
+        self.userauth_password_ex = self.setup('userauth_password_ex', [POINTER(LibSsh2Session), c_char_p, c_uint, c_char_p, c_uint, c_void_p], c_int)
         #int libssh2_userauth_password(LIBSSH2_SESSION *session,   const char *username,   const char *password);
         self.userauth_password = lambda session, username, password: self.userauth_password_ex(session, username, len(username), password, len(password), None)
         
         #========== LIBSSH2_CHANNEL
         #LIBSSH2_CHANNEL * libssh2_channel_open_ex(LIBSSH2_SESSION *session, const char *channel_type, unsigned int channel_type_len, unsigned int window_size, unsigned int packet_size, const char *message, unsigned int message_len);
-        self.channel_open_ex = self.setup('channel_open_ex', [POINTER(Session), c_char_p, c_uint, c_uint, c_uint, c_char_p, c_uint], POINTER(Channel))
+        self.channel_open_ex = self.setup('channel_open_ex', [POINTER(LibSsh2Session), c_char_p, c_uint, c_uint, c_uint, c_char_p, c_uint], POINTER(LibSsh2Channel))
         #LIBSSH2_CHANNEL * libssh2_channel_open_session(session);
         self.channel_open_session = lambda session: self.channel_open_ex(session, 'session', len('session'), 256*1024, 32768, "", len(""))
         #int libssh2_channel_close(LIBSSH2_CHANNEL *channel);
-        self.channel_close = self.setup('channel_close', [POINTER(Channel)], c_int)
+        self.channel_close = self.setup('channel_close', [POINTER(LibSsh2Channel)], c_int)
         #int libssh2_channel_free(LIBSSH2_CHANNEL *channel);
-        self.channel_free = self.setup('channel_free', [POINTER(Channel)], c_int)
+        self.channel_free = self.setup('channel_free', [POINTER(LibSsh2Channel)], c_int)
         #int libssh2_channel_process_startup(LIBSSH2_CHANNEL *channel,   const char *request,   unsigned int request_len,   const char *message,   unsigned int message_len);
-        self.channel_process_startup = self.setup('channel_process_startup', [POINTER(Channel), c_char_p, c_uint, c_char_p, c_uint], c_int)
+        self.channel_process_startup = self.setup('channel_process_startup', [POINTER(LibSsh2Channel), c_char_p, c_uint, c_char_p, c_uint], c_int)
         #int libssh2_channel_exec(LIBSSH2_CHANNEL *channel, const char *command);
         self.channel_exec = lambda channel, command: self.channel_process_startup(channel, "exec", len("exec"), command, len(command))
         #ssize_t libssh2_channel_read_ex(LIBSSH2_CHANNEL *channel, int stream_id, char *buf, size_t buflen);
-        self.channel_read_ex = self.setup('channel_read_ex', [POINTER(Channel), c_int, c_char_p, c_size_t], c_ssize_t)
+        self.channel_read_ex = self.setup('channel_read_ex', [POINTER(LibSsh2Channel), c_int, c_char_p, c_size_t], c_ssize_t)
         #ssize_t libssh2_channel_read(LIBSSH2_CHANNEL *channel, char *buf, size_t buflen);
         self.channel_read = lambda channel, buf: self.channel_read_ex(channel, 0, buf, len(buf))
         #ssize_t libssh2_channel_read_stderr(LIBSSH2_CHANNEL *channel, char *buf, size_t buflen);
@@ -104,19 +104,19 @@ class LibSsh2:
         
         #========== LIBSSH2_AGENT
         #LIBSSH2_AGENT *libssh2_agent_init(LIBSSH2_SESSION *session);
-        self.agent_init = self.setup('agent_init', [POINTER(Session)], POINTER(Agent))
+        self.agent_init = self.setup('agent_init', [POINTER(LibSsh2Session)], POINTER(LibSsh2Agent))
         #int libssh2_agent_connect(LIBSSH2_AGENT *agent);
-        self.agent_connect = self.setup('agent_connect', [POINTER(Agent)], c_int)
+        self.agent_connect = self.setup('agent_connect', [POINTER(LibSsh2Agent)], c_int)
         #int libssh2_agent_list_identities(LIBSSH2_AGENT *agent);
-        self.agent_list_identities = self.setup('agent_list_identities', [POINTER(Agent)], c_int)
+        self.agent_list_identities = self.setup('agent_list_identities', [POINTER(LibSsh2Agent)], c_int)
         #int libssh2_agent_get_identity(LIBSSH2_AGENT *agent,   struct libssh2_agent_publickey **store,   struct libssh2_agent_publickey *prev);
-        self.agent_get_identity = self.setup('agent_get_identity', [POINTER(Agent), POINTER(POINTER(AgentPublicKey)), POINTER(AgentPublicKey)], c_int)
+        self.agent_get_identity = self.setup('agent_get_identity', [POINTER(LibSsh2Agent), POINTER(POINTER(LibSsh2AgentPublicKey)), POINTER(LibSsh2AgentPublicKey)], c_int)
         #int libssh2_agent_userauth(LIBSSH2_AGENT *agent,   const char *username,   struct libssh2_agent_publickey *identity);
-        self.agent_userauth = self.setup('agent_userauth', [POINTER(Agent), c_char_p, POINTER(AgentPublicKey)], c_int)
+        self.agent_userauth = self.setup('agent_userauth', [POINTER(LibSsh2Agent), c_char_p, POINTER(LibSsh2AgentPublicKey)], c_int)
         #int libssh2_agent_disconnect(LIBSSH2_AGENT *agent);
-        self.agent_disconnect = self.setup('agent_disconnect', [POINTER(Agent)], c_int)
+        self.agent_disconnect = self.setup('agent_disconnect', [POINTER(LibSsh2Agent)], c_int)
         #void libssh2_agent_free(LIBSSH2_AGENT *agent);
-        self.agent_free = self.setup('agent_free', [POINTER(Agent)], None)
+        self.agent_free = self.setup('agent_free', [POINTER(LibSsh2Agent)], None)
     
     def __del__(self):
         self.exit()
