7e4ceacfb2232ef6d42e280eaf88684fb9518344
btibi <ihrwein@gmail.com>
2014-08-30 17:21:50 +0200

    setup: add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyssh2',
+      version='1.0',
+      description='SSH library for Python3',
+      url='https://github.com/ihrwein/PySsh2',
+      packages=['pyssh2'],
+)
